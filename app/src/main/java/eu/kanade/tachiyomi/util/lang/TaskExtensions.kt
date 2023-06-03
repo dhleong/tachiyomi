316ecd013e638c9126479030bcfba428fdf62a03
Daniel Leong <me@dhleong.net>
2023-06-03 12:20:00 -0400

    Support customizing the result granularity level

--- app/src/main/java/eu/kanade/tachiyomi/util/lang/TaskExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/lang/TaskExtensions.kt
@@ -1,6 +1,5 @@
 package eu.kanade.tachiyomi.util.lang
 
-import android.util.Log
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
@@ -8,7 +7,6 @@ import kotlin.coroutines.resumeWithException
 
 suspend fun <TResult> Task<TResult>.await(): TResult {
     return suspendCancellableCoroutine { continuation ->
-        Log.v("ml", "TASK=$this / ${this.javaClass}")
         continuation.invokeOnCancellation {
             // Is it possible to cancel the task?
         }
